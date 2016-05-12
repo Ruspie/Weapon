634216fc10f3760726541a641bf485cb5e2e4c5f
Alex_Begun <begun4ik111@mail.ru>
2016-05-12 23:38:58 +0300

    add

---
+++ Weapon/Classes/Parent Classes/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon.Classes.Parent_Classes
+{
+    abstract class Weapon
+    {
+        public string Manufacturer { get; set; }
+        public string Owner { get; set; }
+        public int Weight { get; set; }
+        public string Material { get; set; }
+    }
+}
