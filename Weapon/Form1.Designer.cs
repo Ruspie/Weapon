0a4ba229b7e57a4f9bf957f561eaabf570c1f695
Alex_Begun <begun4ik111@mail.ru>
2016-05-14 05:05:11 +0300

    finish

--- Weapon/Form1.Designer.cs
+++ Weapon/Form1.Designer.cs
@@ -36,16 +36,26 @@
             this.comboBoxSetProperties = new System.Windows.Forms.ComboBox();
             this.groupBoxSelectedWeaponSet = new System.Windows.Forms.GroupBox();
             this.comboBoxWeapon = new System.Windows.Forms.ComboBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.groupBoxListWeapons = new System.Windows.Forms.GroupBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.textBoxGetValue = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
             this.comboBoxListWeapon = new System.Windows.Forms.ComboBox();
+            this.groupBoxSelectWeaponInList = new System.Windows.Forms.GroupBox();
+            this.groupBoxSelectProperty = new System.Windows.Forms.GroupBox();
+            this.comboBoxSelectProperty = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonSerialize = new System.Windows.Forms.Button();
+            this.buttonDeserialize = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBoxValueSet.SuspendLayout();
             this.groupBoxSelectedPropertySet.SuspendLayout();
             this.groupBoxSelectedWeaponSet.SuspendLayout();
+            this.groupBoxListWeapons.SuspendLayout();
+            this.groupBoxSelectWeaponInList.SuspendLayout();
+            this.groupBoxSelectProperty.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -128,67 +138,133 @@
             this.comboBoxWeapon.TabIndex = 1;
             this.comboBoxWeapon.SelectedIndexChanged += new System.EventHandler(this.comboBoxWeapon_SelectedIndexChanged);
             // 
-            // groupBox2
+            // groupBoxListWeapons
             // 
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.textBox2);
-            this.groupBox2.Controls.Add(this.comboBox5);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.comboBoxListWeapon);
-            this.groupBox2.Location = new System.Drawing.Point(12, 238);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(305, 201);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBoxListWeapons.Controls.Add(this.groupBox3);
+            this.groupBoxListWeapons.Controls.Add(this.groupBoxSelectProperty);
+            this.groupBoxListWeapons.Controls.Add(this.groupBoxSelectWeaponInList);
+            this.groupBoxListWeapons.Controls.Add(this.buttonChange);
+            this.groupBoxListWeapons.Controls.Add(this.buttonDelete);
+            this.groupBoxListWeapons.Location = new System.Drawing.Point(12, 238);
+            this.groupBoxListWeapons.Name = "groupBoxListWeapons";
+            this.groupBoxListWeapons.Size = new System.Drawing.Size(474, 223);
+            this.groupBoxListWeapons.TabIndex = 1;
+            this.groupBoxListWeapons.TabStop = false;
+            this.groupBoxListWeapons.Text = "List Weapons :";
             // 
-            // button2
+            // buttonChange
             // 
-            this.button2.Location = new System.Drawing.Point(58, 180);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonChange.Location = new System.Drawing.Point(328, 192);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(75, 23);
+            this.buttonChange.TabIndex = 4;
+            this.buttonChange.Text = "Change";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
             // 
-            // textBox2
+            // textBoxGetValue
             // 
-            this.textBox2.Location = new System.Drawing.Point(21, 108);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(153, 20);
-            this.textBox2.TabIndex = 3;
+            this.textBoxGetValue.Location = new System.Drawing.Point(6, 19);
+            this.textBoxGetValue.Name = "textBoxGetValue";
+            this.textBoxGetValue.Size = new System.Drawing.Size(441, 20);
+            this.textBoxGetValue.TabIndex = 3;
             // 
-            // comboBox5
+            // buttonDelete
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(21, 72);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(153, 21);
-            this.comboBox5.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(58, 143);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonDelete.Location = new System.Drawing.Point(48, 192);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // comboBoxListWeapon
             // 
             this.comboBoxListWeapon.FormattingEnabled = true;
-            this.comboBoxListWeapon.Location = new System.Drawing.Point(21, 35);
+            this.comboBoxListWeapon.Location = new System.Drawing.Point(6, 19);
             this.comboBoxListWeapon.Name = "comboBoxListWeapon";
-            this.comboBoxListWeapon.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxListWeapon.Size = new System.Drawing.Size(441, 21);
             this.comboBoxListWeapon.TabIndex = 0;
+            this.comboBoxListWeapon.SelectedIndexChanged += new System.EventHandler(this.comboBoxListWeapon_SelectedIndexChanged);
+            // 
+            // groupBoxSelectWeaponInList
+            // 
+            this.groupBoxSelectWeaponInList.Controls.Add(this.comboBoxListWeapon);
+            this.groupBoxSelectWeaponInList.Location = new System.Drawing.Point(6, 19);
+            this.groupBoxSelectWeaponInList.Name = "groupBoxSelectWeaponInList";
+            this.groupBoxSelectWeaponInList.Size = new System.Drawing.Size(457, 52);
+            this.groupBoxSelectWeaponInList.TabIndex = 5;
+            this.groupBoxSelectWeaponInList.TabStop = false;
+            this.groupBoxSelectWeaponInList.Text = "Select Weapon in List :";
+            // 
+            // groupBoxSelectProperty
+            // 
+            this.groupBoxSelectProperty.Controls.Add(this.comboBoxSelectProperty);
+            this.groupBoxSelectProperty.Location = new System.Drawing.Point(6, 77);
+            this.groupBoxSelectProperty.Name = "groupBoxSelectProperty";
+            this.groupBoxSelectProperty.Size = new System.Drawing.Size(457, 52);
+            this.groupBoxSelectProperty.TabIndex = 5;
+            this.groupBoxSelectProperty.TabStop = false;
+            this.groupBoxSelectProperty.Text = "Select Property :";
+            // 
+            // comboBoxSelectProperty
+            // 
+            this.comboBoxSelectProperty.FormattingEnabled = true;
+            this.comboBoxSelectProperty.Location = new System.Drawing.Point(6, 19);
+            this.comboBoxSelectProperty.Name = "comboBoxSelectProperty";
+            this.comboBoxSelectProperty.Size = new System.Drawing.Size(441, 21);
+            this.comboBoxSelectProperty.TabIndex = 1;
+            this.comboBoxSelectProperty.SelectedIndexChanged += new System.EventHandler(this.comboBoxSelectProperty_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxGetValue);
+            this.groupBox3.Location = new System.Drawing.Point(6, 135);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(457, 51);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Set Value :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonDeserialize);
+            this.groupBox2.Controls.Add(this.buttonSerialize);
+            this.groupBox2.Location = new System.Drawing.Point(12, 467);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(474, 52);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sere";
+            // 
+            // buttonSerialize
+            // 
+            this.buttonSerialize.Location = new System.Drawing.Point(48, 19);
+            this.buttonSerialize.Name = "buttonSerialize";
+            this.buttonSerialize.Size = new System.Drawing.Size(75, 23);
+            this.buttonSerialize.TabIndex = 8;
+            this.buttonSerialize.Text = "Serialize";
+            this.buttonSerialize.UseVisualStyleBackColor = true;
+            this.buttonSerialize.Click += new System.EventHandler(this.buttonSerialize_Click);
+            // 
+            // buttonDeserialize
+            // 
+            this.buttonDeserialize.Location = new System.Drawing.Point(328, 19);
+            this.buttonDeserialize.Name = "buttonDeserialize";
+            this.buttonDeserialize.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeserialize.TabIndex = 8;
+            this.buttonDeserialize.Text = "Deserialize";
+            this.buttonDeserialize.UseVisualStyleBackColor = true;
+            this.buttonDeserialize.Click += new System.EventHandler(this.buttonDeserialize_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1243, 533);
+            this.ClientSize = new System.Drawing.Size(495, 525);
             this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBoxListWeapons);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -198,8 +274,12 @@
             this.groupBoxValueSet.PerformLayout();
             this.groupBoxSelectedPropertySet.ResumeLayout(false);
             this.groupBoxSelectedWeaponSet.ResumeLayout(false);
+            this.groupBoxListWeapons.ResumeLayout(false);
+            this.groupBoxSelectWeaponInList.ResumeLayout(false);
+            this.groupBoxSelectProperty.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -210,16 +290,22 @@
         private System.Windows.Forms.TextBox textBoxSetValue;
         private System.Windows.Forms.ComboBox comboBoxSetProperties;
         private System.Windows.Forms.ComboBox comboBoxWeapon;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBoxListWeapons;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.TextBox textBoxGetValue;
+        private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.ComboBox comboBoxListWeapon;
         private System.Windows.Forms.GroupBox groupBoxSelectedWeaponSet;
         private System.Windows.Forms.GroupBox groupBoxSelectedPropertySet;
         private System.Windows.Forms.GroupBox groupBoxValueSet;
         private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.GroupBox groupBoxSelectProperty;
+        private System.Windows.Forms.ComboBox comboBoxSelectProperty;
+        private System.Windows.Forms.GroupBox groupBoxSelectWeaponInList;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonDeserialize;
+        private System.Windows.Forms.Button buttonSerialize;
     }
 }
 
--- Weapon/Form1.cs
+++ Weapon/Form1.cs
@@ -1,52 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Weapon.Classes;
 using Weapon.Classes.Children_Classes;
-using Weapon.Classes.Parent_Classes;
 
 namespace Weapon
 {
     public partial class Form1 : Form
     {
-        private LogWeapons logWeapons = new LogWeapons();
+        private readonly LogWeapons _logWeapons = new LogWeapons();
         private Classes.Parent_Classes.Weapon _weapon;
         public Form1()
         {
             InitializeComponent();
         }
 
-       /* private void button2_Click(object sender, EventArgs e)
-        {
-            BinaryFormatter serialization = new BinaryFormatter();
-
-            using (FileStream fs = new FileStream("log.bin", FileMode.OpenOrCreate))
-            {
-                serialization.Serialize(fs, logWeapons);
-                MessageBox.Show("Serialization Complete");
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            BinaryFormatter serialization = new BinaryFormatter();
-
-            using (FileStream fs = new FileStream("log.bin", FileMode.Open))
-            {
-                logWeapons = (List<Classes.Parent_Classes.Weapon>)serialization.Deserialize(fs);
-            }
-        }*/
-
-        private void ClearComboBox(ComboBox comboBox)
+       private void ClearComboBox(ComboBox comboBox)
         {
             while (comboBox.Items.Count > 0)
             {
@@ -67,31 +39,32 @@ namespace Weapon
         private Classes.Parent_Classes.Weapon CreateWeapon()
         {
             CreatorWeapons creatorWeapons = new CreatorWeapons();
+            Classes.Parent_Classes.Weapon weapon;
             switch (comboBoxWeapon.SelectedIndex)
             {
                 case 0:
-                    _weapon = creatorWeapons.CreateWeapon(new Bow(), "", "", 0, "", 0, "");
+                    weapon = creatorWeapons.CreateWeapon(new Bow(), "", "", 0, "", 0, "");
                     break;
                 case 1:
-                    _weapon = creatorWeapons.CreateWeapon(new Dagger(), "", "", 0, "", 0, "", "");
+                    weapon = creatorWeapons.CreateWeapon(new Dagger(), "", "", 0, "", 0, "", "");
                     break;
                 case 2:
-                    _weapon = creatorWeapons.CreateWeapon(new Grenade(), "", "", 0, "", 0, "");
+                    weapon = creatorWeapons.CreateWeapon(new Grenade(), "", "", 0, "", 0, "");
                     break;
                 case 3:
-                    _weapon = creatorWeapons.CreateWeapon(new Pistol(), "", "", 0, "", 0, "");
+                    weapon = creatorWeapons.CreateWeapon(new Pistol(), "", "", 0, "", 0, "");
                     break;
                 case 4:
-                    _weapon = creatorWeapons.CreateWeapon(new Rifle(), "", "", 0, "", 0, "");
+                    weapon = creatorWeapons.CreateWeapon(new Rifle(), "", "", 0, "", 0, "");
                     break;
                 case 5:
-                    _weapon = creatorWeapons.CreateWeapon(new Sword(), "", "", 0, "", 0, "", "");
+                    weapon = creatorWeapons.CreateWeapon(new Sword(), "", "", 0, "", 0, "", "");
                     break;
                 default:
-                    _weapon = creatorWeapons.CreateWeapon(new Bow(), "", "", 0, "", 0, "");
+                    weapon = creatorWeapons.CreateWeapon(new Bow(), "", "", 0, "", 0, "");
                     break;
             }
-            return _weapon;
+            return weapon;
         }
 
        private void comboBoxWeapon_SelectedIndexChanged(object sender, EventArgs e)
@@ -107,15 +80,16 @@ namespace Weapon
 
         private void button_Click(object sender, EventArgs e)
         {
-            logWeapons.ListWeapons.Add(_weapon);
+            _logWeapons.ListWeapons.Add(_weapon);
             comboBoxListWeapon.Items.Add(_weapon);
-            comboBoxListWeapon.SelectedIndex = 1;
-            comboBoxWeapon.SelectedIndex = 0;
+            comboBoxListWeapon.SelectedIndex = 0;
+            _weapon = CreateWeapon();
+            textBoxSetValue.Clear();
         }
 
-        private string GetNameProperty()
+        private string GetNameProperty(ComboBox comboBox)
         {
-            string propertiesName = comboBoxSetProperties.Text;
+            string propertiesName = comboBox.Text;
             int ind = propertiesName.LastIndexOf(" ", StringComparison.Ordinal);
             propertiesName = propertiesName.Substring(ind + 1);
             return propertiesName;
@@ -125,19 +99,19 @@ namespace Weapon
         {
             try
             {
-                if (_weapon.GetType().GetProperty(GetNameProperty()).PropertyType == typeof(float))
+                if (_weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).PropertyType == typeof(float))
                 {
-                    _weapon.GetType().GetProperty(GetNameProperty()).SetValue(_weapon, Convert.ToSingle(textBoxSetValue.Text));
+                    _weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).SetValue(_weapon, Convert.ToSingle(textBoxSetValue.Text));
                 }
                 else
                 {
-                    if (_weapon.GetType().GetProperty(GetNameProperty()).PropertyType == typeof(int))
+                    if (_weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).PropertyType == typeof(int))
                     {
-                        _weapon.GetType().GetProperty(GetNameProperty()).SetValue(_weapon, Convert.ToInt32(textBoxSetValue.Text));
+                        _weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).SetValue(_weapon, Convert.ToInt32(textBoxSetValue.Text));
                     }
                     else
                     {
-                        _weapon.GetType().GetProperty(GetNameProperty()).SetValue(_weapon, textBoxSetValue.Text);
+                        _weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).SetValue(_weapon, textBoxSetValue.Text);
                     }
                 }
             }
@@ -149,7 +123,104 @@ namespace Weapon
 
         private void comboBoxSetProperties_SelectedIndexChanged(object sender, EventArgs e)
         {
-            textBoxSetValue.Text = Convert.ToString(_weapon.GetType().GetProperty(GetNameProperty()).GetValue(_weapon));
+            textBoxSetValue.Text = Convert.ToString(_weapon.GetType().GetProperty(GetNameProperty(comboBoxSetProperties)).GetValue(_weapon));
+        }
+
+        private void comboBoxListWeapon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearComboBox(comboBoxSelectProperty);
+            foreach (PropertyInfo key in _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType().GetProperties())
+            {
+                comboBoxSelectProperty.Items.Add(key);
+            }
+            comboBoxSelectProperty.SelectedIndex = 0;
+        }
+
+        private void comboBoxSelectProperty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxGetValue.Text = Convert.ToString(
+                _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                    .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                    .GetValue(_logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex]));
+        }
+
+        private void buttonSerialize_Click(object sender, EventArgs e)
+        {
+            BinaryFormatter serialization = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("log.bin", FileMode.OpenOrCreate))
+            {
+                serialization.Serialize(fs, _logWeapons.ListWeapons);
+                MessageBox.Show("Serialization Complete");
+            }
+        }
+
+        private void CreateListComboBox(ComboBox comboBox)
+        {
+            foreach (var weapon in _logWeapons.ListWeapons)
+            {
+                //comboBox.Items.Add(weapon.GetType().GetProperties());
+                comboBox.Items.Add((weapon).GetType());
+            }
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void buttonDeserialize_Click(object sender, EventArgs e)
+        {
+            BinaryFormatter serialization = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("log.bin", FileMode.Open))
+            {
+                _logWeapons.ListWeapons = (List<Classes.Parent_Classes.Weapon>)serialization.Deserialize(fs);
+            }
+            ClearComboBox(comboBoxListWeapon);
+            CreateListComboBox(comboBoxListWeapon);
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (
+                    _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                        .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                        .PropertyType == typeof (float))
+                {
+                    _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                        .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                        .SetValue(_logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex],
+                            Convert.ToSingle(textBoxGetValue.Text));
+                }
+                else
+                {
+                    if (
+                        _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                            .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                            .PropertyType == typeof (int))
+                    {
+                        _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                            .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                            .SetValue(_logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex],
+                                Convert.ToInt32(textBoxGetValue.Text));
+                    }
+                    else
+                    {
+                        _logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex].GetType()
+                            .GetProperty(GetNameProperty(comboBoxSelectProperty))
+                            .SetValue(_logWeapons.ListWeapons[comboBoxListWeapon.SelectedIndex], textBoxGetValue.Text);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Failed to write data");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            _logWeapons.ListWeapons.RemoveAt(comboBoxListWeapon.SelectedIndex);
+            comboBoxListWeapon.Items.RemoveAt(comboBoxListWeapon.SelectedIndex);            
         }
     }
 }
